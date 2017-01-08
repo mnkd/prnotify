7d84548f2f8e9bfe79b50d45b2686a6bf1c6594f
m-nakada <m-nakada@nekojarashi.com>
2017-01-08 09:37:14 +0900

    Support print version

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 )
 
@@ -11,16 +12,28 @@ const (
 	ExitCodeFileError
 )
 
+var (
+	Version  string
+	Revision string
+)
+
 var app App
 
 func init() {
 	var configPath string
 	var index int
-	var dryRun bool
+	var dryRun, version bool
 	flag.StringVar(&configPath, "c", "", "/path/to/config.json. (default: $HOME/.config/prnotify/config.json)")
 	flag.IntVar(&index, "swi", 0, "Slack Webhooks Index (default: 0)")
 	flag.BoolVar(&dryRun, "d", false, "A dry run will not send any message to Slack. (defualt: false)")
+	flag.BoolVar(&version, "v", false, "Print version.")
 	flag.Parse()
+
+	if version {
+		fmt.Fprintln(os.Stdout, "Version:", Version)
+		fmt.Fprintln(os.Stdout, "Revision:", Revision)
+		os.Exit(ExitCodeOK)
+	}
 
 	// Prepare config
 	config, err := NewConfig(configPath, index, dryRun)
